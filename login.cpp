592f0099663fc81091c5725c8e2b761d735df297
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 22:05:29 +0530

    lock acccount function

--- login.cpp
+++ login.cpp
@@ -7,6 +7,12 @@ string password="123";
 int maxAttempts=5;
 int failedAttempts=0;
 
+void lockAccount()
+{
+    cout<<"\nAccount has been locked\n";
+    cout<<"\nAlerting the user";
+}
+
 void loginAttempts()
 {
     if(failedAttempts>maxAttempts)
@@ -23,5 +29,5 @@ int main()
 {
     cout<<"-----Login Process-----\n";
     loginAttempts();
-
+    cout<<"\n------Process Completed-----\n";
 }
