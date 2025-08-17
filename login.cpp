f363c90317910a632348041621235678e86b316e
pranavrajesh23 <pranav.rajesh@psiog.com>
2025-08-17 22:11:27 +0530

    login function

--- login.cpp
+++ login.cpp
@@ -2,10 +2,33 @@
 using namespace std;
 
 string USER="pranav";
-string password="123";
+string PASS="123";
 
 int maxAttempts=5;
 int failedAttempts=0;
+
+void login()
+{
+    cout<<"\nLogin\n";
+    cout<<"\nEnter your name: \n";
+    string user;
+    cin>>user;
+    cout<<"\nEnter your password: \n";
+    string pass;
+    cin>>pass;
+    cout<<"\nIs this a authorized account?\n";
+    if(USER==user && PASS==pass)
+    {
+        cout<<"\nYes, this is a authorized account\n";
+        cout<<"\nGrant access\n";
+    }
+    else
+    {
+        failedAttempts++;
+        cout<<"\nTry again\n";
+        loginAttempts();
+    }
+}
 
 void lockAccount()
 {
